f563e547972bfe6782f82e7f157b7348cc578ecd
Çağıl Uluşahin <cagilulusahin@gmail.com>
2017-06-29 20:41:46 +0300

    class added

--- events/prepare_datafile.py
+++ events/prepare_datafile.py
@@ -1,4 +1,4 @@
-import os,sys
+import os,sys,random
 import numpy as np
 from xml.etree import ElementTree
 from data_conf import *
@@ -8,8 +8,7 @@ from nltk import word_tokenize, sent_tokenize
 from collections import defaultdict
 py3 = version_info[0] > 2 #creates boolean value for test that Python major version > 2
 if not py3:
-  input = raw_input
-vocab = defaultdict(float)
+    input = raw_input
 
 # source: https://github.com/anoperson/jointEE-NN/blob/master/jee_processData.py#L353
 def load_bin_vec(vocab,fname='/datasets/GoogleNews-vectors-negative300.bin'):
@@ -21,8 +20,8 @@ def load_bin_vec(vocab,fname='/datasets/GoogleNews-vectors-negative300.bin'):
     word_vecs = {}
     dim = 0
     for word in vocab:
-      word_vecs[word] = model.word_vec("all")
-      dim = word_vecs[word].shape[0]
+        word_vecs[word] = model.word_vec("all")
+        dim = word_vecs[word].shape[0]
     print('dim: ', dim)
     return dim, word_vecs
 
@@ -46,101 +45,178 @@ def spans(txt):
     tokens=word_tokenize(txt)
     offset = 0
     for token in tokens:
-      if token not in ["''",'``']:
-        offset = txt.find(token, offset)
-        yield token, offset, offset+len(token)
-        offset += len(token)
+        if token not in ["''",'``']:
+            offset = txt.find(token, offset)
+            yield token, offset, offset+len(token)
+            offset += len(token)
 
 def tokenize_with_span(source_str):
-  for token in spans(source_str):
-      yield token
-      assert token[0]==source_str[token[1]:token[2]]
+    for token in spans(source_str):
+        yield token
+        assert token[0]==source_str[token[1]:token[2]]
 
-# <span style="color: red">{}</span>
-def prepare_datafile(ann_filename,source_filename,datafile):
-    with open(source_filename) as input:
-        source_str = input.read()
-    tree = ElementTree.parse(ann_filename)
-    root = tree.getroot()
-    offsets = {}
-    hoppers = root.find("hoppers")
-    for hopper in hoppers:
-        event_mentions = hopper.getchildren()
-        for em in event_mentions:
-            trigger = em.find("trigger")
-            id = em.get("id")
-            subtype = em.get('subtype') #em.get("type")
-            offsets[trigger.get('offset')] = (trigger.get('length'),id,subtype)
-    # offsets have event trigger offsets, length and id
+class Dataset(object):
+    project_folder = PROJECT_FOLDER
+    training_source_folder = os.path.join(project_folder,"data/LDC2017E02/data/2016/eval/eng/nw/source/")
+    training_ann_folder = os.path.join(project_folder,"data/LDC2017E02/data/2016/eval/eng/nw/ere/")
+    test_source_folder = os.path.join(project_folder,"data/LDC2017E02/data/2016/eval/eng/df/source/")
+    test_ann_folder = os.path.join(project_folder,"data/LDC2017E02/data/2016/eval/eng/df/ere/")
+    training_dataset_file = os.path.join(project_folder,"data/dataset_file_training.txt")
+    test_dataset_file =  os.path.join(project_folder,"data/dataset_file_test.txt")
+    vocab = defaultdict(float)
+    training_set, test_set = None, None
+    label_set = ['None','broadcast', 'injure', 'transportperson', 'transfermoney', 'artifact', 'contact',
+                 'elect', 'correspondence', 'startposition', 'transportartifact', 'demonstrate', 'arrestjail',
+                 'meet', 'transferownership', 'transaction', 'die', 'attack', 'endposition']
+    def __init__(self, vocab_filename=None,training_dataset_file=None,test_dataset_file=None):
+        if vocab_filename:
+            read_vocab(self,vocab_filename)
+        if training_dataset_file:
+            self.training_dataset_file = training_dataset_file
+        if test_dataset_file:
+            self.test_dataset_file = test_dataset_file
 
-    tokenized_source = tokenize_with_span(source_str)
+    def process(self):
+        self.prepare_dataset_file(self.training_dataset_file,self.training_source_folder, self.training_ann_folder)
+        self.prepare_dataset_file(self.test_dataset_file,self.test_source_folder, self.test_ann_folder)
+        self.build_dataset()
+        self.show_label_percentage()
 
-    with open(datafile,"a") as output:
-      for token_span in tokenized_source:
-        subtype = None
-        if str(token_span[1]) in offsets.keys():
-          subtype = offsets.pop(str(token_span[1]))[2]
-        word = token_span[0]
-        print("%s\t%s\t%s\t%s" %(word,token_span[1],token_span[2],subtype), file=output)
-        vocab[word] += 1
-    return vocab
+    def build_dataset(self):
+        self.training_set = self.load_data_and_labels(self.training_dataset_file)
+        self.test_set = self.load_data_and_labels(self.test_dataset_file)
 
-def prepare_test_data(datafile):
-    results = []
-    file_index = 0
-    #vocab = defaultdict(float)
+    def set_training_folders(self,training_source_folder, training_ann_folder):
+        self.training_source_folder, self.training_ann_folder = training_source_folder, training_ann_folder
 
-    project_folder = os.path.abspath(os.path.join(os.path.abspath(os.curdir), os.pardir))
-    source_folder = os.path.join(project_folder,"data/LDC2017E02/data/2016/eval/eng/df/source/")
-    ere_folder = os.path.join(project_folder,"data/LDC2017E02/data/2016/eval/eng/df/ere/")
-    ann_filelist = os.listdir(ere_folder)
-    source_filelist = os.listdir(source_folder)
-    ann_filename_fun = lambda x:  os.path.join(ere_folder,ann_filelist[x])
-    source_filename_fun = lambda x:  os.path.join(source_folder,source_filelist[x])
+    def set_test_folders(self,test_source_folder, test_ann_folder):
+        self.test_source_folder, self.test_ann_folder = test_sourse_folder, test_ann_folder,
+
+    def write_vocab(self,vocab_filename):
+        with open(vocab_filename,"w") as v_file:
+            for key, value in self.vocab.items():
+                v_file.write("%s\t%s\n" % (key,value))
+
+    def read_vocab(self,vocab_filename):
+        with open(vocab_filename,"w") as v_file:
+            for line in iter(v_file.readline, ''):
+                [word,_] = line.split("\t")
+                self.vocab.append(word)
+
+    def update_vocab(self,source_filename):
+        tree = ElementTree.parse(source_filename)
+        root = tree.getroot()
+        for child in root.getchildren():
+            #print(child.text.strip())
+            for par in child.text.strip().split("\n"):
+                if par:
+                    sentences = sent_tokenize(par)
+                    for sent in sentences:
+                        words = word_tokenize(sent)
+                        for word in words:
+                            self.vocab[word] += 1
+
+    # <span style="color: red">{}</span>
+    def process_datafile(self,ann_filename,source_filename,dataset_file):
+        with open(source_filename) as input:
+            source_str = input.read()
+        self.update_vocab(source_filename)
+        tree = ElementTree.parse(ann_filename)
+        root = tree.getroot()
+        offsets = {}
+        hoppers = root.find("hoppers")
+        for hopper in hoppers:
+            event_mentions = hopper.getchildren()
+            for em in event_mentions:
+                trigger = em.find("trigger")
+                id = em.get("id")
+                subtype = em.get('subtype') #em.get("type")
+                offsets[trigger.get('offset')] = (trigger.get('length'),id,subtype)
+        # offsets have event trigger offsets, length and id
+
+        tokenized_source = tokenize_with_span(source_str)
+        with open(dataset_file,"a") as output:
+            for token_span in tokenized_source:
+                subtype = None
+                word = token_span[0]
+                if word in self.vocab:
+                    if str(token_span[1]) in offsets.keys():
+                        subtype = offsets.pop(str(token_span[1]))[2]
+                    print("%s\t%s\t%s\t%s" %(word,token_span[1],token_span[2],subtype), file=output)
+
+    # writes dataset to file
+    def prepare_dataset_file(self,dataset_file,
+                          source_folder=os.path.join(PROJECT_FOLDER,"data/LDC2017E02/data/2016/eval/eng/df/source/"),
+                          ere_folder = os.path.join(PROJECT_FOLDER,"data/LDC2017E02/data/2016/eval/eng/df/ere/")):
+        """
+        Takes source folder, annotation folder and dataset file to write the data as arguments.
+        Reads dataset from source and annotation folder and writes the data to dataset file.
+        """
+        try:
+            os.remove(dataset_file)
+        except FileNotFoundError:
+            pass
+        file_index = 0
+        ann_filelist = os.listdir(ere_folder)
+        source_filelist = os.listdir(source_folder)
+        ann_filename_fun = lambda x:  os.path.join(ere_folder,ann_filelist[x])
+        source_filename_fun = lambda x:  os.path.join(source_folder,source_filelist[x])
+
+        list_dir = os.listdir(source_folder)
+        while file_index < len(list_dir):
+            ann_filename = ann_filename_fun(file_index)
+            source_filename = source_filename_fun(file_index)
+            self.process_datafile(ann_filename,source_filename,dataset_file)
+            file_index += 1
 
 
-    list_dir = os.listdir(source_folder)
-    while file_index < len(list_dir):
-        ann_filename = ann_filename_fun(file_index)
-        source_filename = source_filename_fun(file_index)
-        prepare_datafile(ann_filename,source_filename,datafile)
-        file_index += 1
-    return vocab
+    def load_data_and_labels(self,dataset_file):
+        dataset = []
+        x,y_text = [],[]
+        with open(dataset_file,"r") as df:
+            for line in iter(df.readline, ''):
+                [word,_,_,subtype] = line.split("\t")
+                if word in self.vocab: # todo should fix unknown words
+                    dataset.append((word,subtype.strip()))
 
+        for line in dataset:
+            if line[1] != 'None' or random.random() < 0.002:
+                x.append(line[0])
+                y_text.append(line[1])
 
-def load_data_and_labels(vocab,datafile=DATAFILE):
-  x,y_text = [],[]
-  with open(datafile,"r") as df:
-    for line in iter(df.readline, ''):
-      [word,_,_,subtype] = line.split("\t")
-      if word in vocab: # todo should fix unknown words
-        x.append(word)
-        y_text.append(subtype.strip())
-  set_y = list(set(y_text))
-  y = [set_y.index(item) for item in y_text]
+        y = [self.label_set.index(item) for item in y_text]
 
-  #for i in range(len(y)):
-  #  assert one_hot[y[i]].all() == one_hot_y[i].all()
-  return x,y
+        #for i in range(len(y)):
+        #  assert one_hot[y[i]].all() == one_hot_y[i].all()
+        return x,y,dataset
+
+    def show_label_percentage(self):
+        training = self.training_set
+        test = self.test_set
+        print("Tr #\tTr %\tTe #\tTe %\tlabel\n")
+        for i in range(0,len(self.label_set)):
+            print("%d\t%.2f\t%d\t%.2f\t%s" %(training[1].count(i),training[1].count(i)/len(training[1]),
+                                             test[1].count(i),test[1].count(i)/len(test[1]),
+                                             self.label_set[i]))
 
 def get_one_hot(y):
-  #one_hot = {}
-  #for i in range(len(set(y))):
-  #  one_hot[set_y[i]] = np.zeros(len(set_y))
-  #  one_hot[set_y[i]][i] = 1
-  #  one_hot_y = [one_hot[item] for item in y]
-  identity = np.identity(16)
-  one_hot_y = [identity[item] for item in y]
-  return one_hot_y
+        #one_hot = {}
+        #for i in range(len(set(y))):
+        #  one_hot[set_y[i]] = np.zeros(len(set_y))
+        #  one_hot[set_y[i]][i] = 1
+        #  one_hot_y = [one_hot[item] for item in y]
+        identity = np.identity(16)
+        one_hot_y = [identity[item] for item in y]
+        return one_hot_y
 
 
 def load_vocab():
-  vocab = []
-  with open(VOCABFILE,"r") as datafile:
-    for line in iter(datafile.readline, ''):
-      [word,_] = line.split("\t")
-      vocab.append(word)
-  return vocab
+    vocab = []
+    with open(VOCABFILE,"r") as datafile:
+      for line in iter(datafile.readline, ''):
+        [word,_] = line.split("\t")
+        vocab.append(word)
+    return vocab
 
 # from https://github.com/dennybritz/cnn-text-classification-tf/blob/master/data_helpers.py#L48
 def batch_iter(data, batch_size, num_epochs, shuffle=True):
@@ -182,11 +258,10 @@ def embeddings():
   embedding = np.asarray(embd)
 
 
-def initialize():
+def initialize(): # todo delete
     """
     Creates datafile and vocab file
     """
-    results = []
     file_index=0
     list_dir = os.listdir(SOURCE_FOLDER)
 
