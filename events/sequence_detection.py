908cd3b49bf539b3bf237add24a4d554f50202b4
Çağıl Uluşahin <cagilulusahin@gmail.com>
2017-09-29 21:15:24 +0300

    write to tbf and fix direction in annotations

--- events/sequence_detection.py
+++ events/sequence_detection.py
@@ -1,4 +1,6 @@
-import nltk,os, numpy as np
+from collections import defaultdict
+
+import datetime,nltk,os, numpy as np
 from data_conf import PROJECT_FOLDER, event_type_index, realis_index
 
 from sklearn.neighbors import KNeighborsClassifier
@@ -11,8 +13,11 @@ from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 
+from sklearn.metrics import recall_score, precision_score, f1_score
+
 from optparse import OptionParser
 
+import random
 SOURCE_FOLDER = os.path.join(PROJECT_FOLDER,"data/LDC2016E130_DEFT_Event_Sequencing_After_Link_Parent_Child_Annotation_Training_Data_V4/data/")
 training_folder = os.path.join(SOURCE_FOLDER,"training")
 
@@ -26,7 +31,7 @@ python sequence_detection.py
 python2 ~/work/EvmEval/scorer_v1.8.py -a SEQUENCING -g /Users/cagil/work/tac_kbp_events/data/LDC2016E130_test.tbf -s /Users/cagil/work/tac_kbp_events/events/run1_results.txt
 """
 
-def add_links(line,events_doc, corefs_doc, afters_doc,parents_doc):
+def read_relations(line,events_doc, corefs_doc, afters_doc,parents_doc):
     _, lid, event_ids = line.strip().split("\t")
     if line.startswith("@After"):
         afters_doc[lid] = event_ids.split(",")
@@ -42,7 +47,7 @@ def add_links(line,events_doc, corefs_doc, afters_doc,parents_doc):
 
 
 # brat_conversion	1b386c986f9d06fd0a0dda70c3b8ade9	E194	145,154	sentences	Justice_Sentence	Actual
-def get_event_dict(ANN_FILE):
+def read_annotations(ANN_FILE):
     events, corefs, afters,parents = {},{},{},{}
     with open(ANN_FILE) as ann_file:
         for line in ann_file:
@@ -53,7 +58,7 @@ def get_event_dict(ANN_FILE):
                 afters[doc_id] = {}
                 parents[doc_id] = {}
             elif line.startswith("@"):
-                add_links(line,events[doc_id], corefs[doc_id], afters[doc_id],parents[doc_id])
+                read_relations(line,events[doc_id], corefs[doc_id], afters[doc_id],parents[doc_id])
             elif line.startswith("b"):
                 _ , _, event_id, offsets, nugget, event_type, realis = line.strip().split("\t")
                 events[doc_id][event_id] = {"offsets":offsets,
@@ -65,17 +70,24 @@ def get_event_dict(ANN_FILE):
                 pass
     return events, corefs, afters,parents
 
-import random
-def get_results_random(events, corefs, afters,parents):
-    run_id = "run1"
+def write_results_tbf(events, corefs, afters,parents,run_id = "run1"):
     results_str = []
     for doc_id in events.keys():
         results_str.append("#BeginOfDocument %s" %doc_id)
         for event_id in events[doc_id].keys():
+            # put events
             results_str.append("\t".join([run_id,doc_id,event_id,events[doc_id][event_id]["offsets"],
                                                events[doc_id][event_id]["nugget"],
                                                events[doc_id][event_id]["event_type"],
                                                events[doc_id][event_id]["realis"]]))
+            # put after links
+        for key1,key2 in afters[doc_id].values():
+            results_str.append("@After\tR11\t%s,%s" % (key1,key2))
+            #results = write_results_after_links_random(events, corefs, afters,parents)
+        results_str.append("#EndOfDocument")
+    print("\n".join(results_str),file=open("%s_results.txt" %run_id,"w"))
+
+def write_results_after_links_random(events, corefs, afters,parents):
         for a in range(1,4):
             try:
                 key1 = random.choice(list(events[doc_id].keys()))
@@ -84,8 +96,6 @@ def get_results_random(events, corefs, afters,parents):
                 results_str.append("@After\tR11\t%s,%s" % (key1,key2))
             except:
                 pass
-        results_str.append("#EndOfDocument")
-    print("\n".join(results_str),file=open("%s_results.txt" %run_id,"w"))
 
 def build_feature_vector(linked_events,events_doc,corefs_doc):
     x = [len(events_doc),len(corefs_doc),]
@@ -102,23 +112,29 @@ def build_feature_vector(linked_events,events_doc,corefs_doc):
     return x
 
 # 'E211' : {'offsets': '1190,1196', 'nugget': 'merged', 'event_type': 'Business_Merge-Org', 'realis': 'Actual'}
-def build_feature_matrix_for_document(doc_id,events_doc, corefs_doc, afters_doc,add_neg=True):
+def build_feature_matrix_for_document(doc_id,events_doc, corefs_doc, afters_doc):
     #print("%s\t%s\t%s\t%s" %(doc_id,len(events_doc),len(corefs_doc),len(afters_doc)))
     #print(set(events_doc))
     X = []
     Y=[]
+    IDS = []
     event_id_list = set(events_doc.keys())
-    while event_id_list:
-        event_id = event_id_list.pop()
+
+    for event_id in event_id_list:
         for to_event_id in event_id_list:
+            #import ipdb ; ipdb.set_trace()
+            # no link definitions between corefs
+            if 'coref' in events_doc[event_id] and to_event_id in corefs_doc[events_doc[event_id]['coref']]:
+                continue
             linked_event_ids = [event_id,to_event_id]
-            x = build_feature_vector(linked_event_ids,events_doc,corefs_doc)
-            X.append(x)
             if linked_event_ids in afters_doc.values():
                 Y.append(1)
             else:
                 Y.append(0)
-    return X,Y
+            x = build_feature_vector(linked_event_ids,events_doc,corefs_doc)
+            X.append(x)
+            IDS.append([doc_id,linked_event_ids[0],linked_event_ids[1]])
+    return X,Y,IDS
 
 def build_feature_matrix_for_document_old(doc_id,events_doc, corefs_doc, afters_doc,add_neg=True):
     #print("%s\t%s\t%s\t%s" %(doc_id,len(events_doc),len(corefs_doc),len(afters_doc)))
@@ -141,19 +157,23 @@ def build_feature_matrix_for_document_old(doc_id,events_doc, corefs_doc, afters_
             x = build_feature_vector(random_ids,events_doc,corefs_doc)
             X.append(x)
             Y.append(0)
+            #IDS.append([doc_id,random_ids[0],random_ids[1]])
             number_of_negative_links += 1
     return X,Y
 
-def get_features(events, corefs, afters,parents,add_neg=True):
+def build_feature_matrix_for_dataset(events, corefs, afters,parents,training=True):
     run_id = "run1"
     results_str = []
     training_X = []
     training_Y = []
+    training_IDS = []
     for doc_id in events.keys():
-        X,Y = build_feature_matrix_for_document(doc_id,events[doc_id],corefs[doc_id],afters[doc_id],add_neg=add_neg)
+        X,Y, IDS = build_feature_matrix_for_document(doc_id,events[doc_id],corefs[doc_id],afters[doc_id])
         training_X.extend(X)
         training_Y.extend(Y)
-    return training_X,training_Y
+        training_IDS.extend(IDS)
+    print("%s set: %s samples" %("Training" if training else "Test", len(training_X)))
+    return training_X,training_Y, training_IDS
 
 def preprocess_dataset(X):
     arr_X = np.array(X,dtype=object)
@@ -168,52 +188,60 @@ def preprocess_dataset(X):
 
     return arr_X
 
-def main():
+def main(debug=False):
     ANN_FILE = os.path.join(PROJECT_FOLDER,"data/LDC2016E130_test.tbf")
-    events, corefs, afters,parents = get_event_dict(ANN_FILE)
+    if debug:
+        import ipdb ; ipdb.set_trace()
+    events, corefs, afters,parents = read_annotations(ANN_FILE)
     get_results(events, corefs, afters,parents)
-    #import ipdb ; ipdb.set_trace()
     #for line in events:
     #    print(line)
 
-def several_classifiers():
-    names = ["Nearest Neighbors", "Linear SVM", "RBF SVM", "Gaussian Process",
-             "Decision Tree", "Random Forest", "Neural Net", "AdaBoost",
-             "Naive Bayes", "QDA"]
-    classifiers = [
-        KNeighborsClassifier(3),
-        #SVC(kernel="linear", C=0.025),
-        #SVC(gamma=2, C=1),
-        GaussianProcessClassifier(1.0 * RBF(1.0), warm_start=True),
-        DecisionTreeClassifier(max_depth=5),
-        RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1),
-        MLPClassifier(alpha=1),
-        AdaBoostClassifier(),
-        GaussianNB(),
-        QuadraticDiscriminantAnalysis()]
+names = ["Nearest Neighbors", "Linear SVM", "RBF SVM", "Gaussian Process",
+         "Decision Tree", "Random Forest", "Neural Net", "AdaBoost",
+         "Naive Bayes", "QDA"]
+classifiers = [
+    KNeighborsClassifier(3),
+    #SVC(kernel="linear", C=0.025),
+    #SVC(gamma=2, C=1),
+    #GaussianProcessClassifier(1.0 * RBF(1.0), warm_start=True),
+    #DecisionTreeClassifier(max_depth=5),
+    #RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1),
+    #MLPClassifier(alpha=1),
+    #AdaBoostClassifier(),
+    GaussianNB(),
+    #QuadraticDiscriminantAnalysis()
+]
 
+def several_classifiers():
     ANN_FILE = os.path.join(PROJECT_FOLDER,"data/LDC2016E130_training.tbf")
-    events, corefs, afters,parents = get_event_dict(ANN_FILE)
-    X_train,y_train = get_features(events, corefs, afters,parents)
+    events, corefs, afters,parents = read_annotations(ANN_FILE)
+    X_train,y_train,_ = build_feature_matrix_for_dataset(events, corefs, afters,parents)
     X_train = preprocess_dataset(X_train)
-    #neigh = KNeighborsClassifier(n_neighbors=3)
-    #neigh.fit(X_train, y_train)
 
     ANN_FILE = os.path.join(PROJECT_FOLDER,"data/LDC2016E130_test.tbf")
-    events, corefs, afters,parents = get_event_dict(ANN_FILE)
-    X_test,y_test = get_features(events, corefs, afters,parents,add_neg=False)
+    events, corefs, afters,parents = read_annotations(ANN_FILE)
+    X_test,y_test, IDS_test = build_feature_matrix_for_dataset(events, corefs, afters,parents,training=False)
     X_test = preprocess_dataset(X_test)
 
-    #neigh.score(X_t,y_test)
-    #import ipdb ; ipdb.set_trace()
-    #print(neigh.predict(X[0:10]))
-    #print(neigh.predict_proba(X[0:10]))
+    #import ipdb ; ipdb.set_trace()    #print(neigh.predict(X[0:10]))    #print(neigh.predict_proba(X[0:10]))
 
     # iterate over classifiers
     for name, clf in zip(names, classifiers):
         clf.fit(X_train, y_train)
-        score = clf.score(X_test, y_test)
-        print("%s: %.4f" %(name,score))
+        y_pred = clf.predict(X_test)
+        links_found = [i for i in range(len(y_pred)) if y_pred[i]]
+        afters_pred = defaultdict(dict)
+
+        for ind in links_found:
+            afters_pred[IDS_test[ind][0]]["R%d" %ind] = [IDS_test[ind][1],IDS_test[ind][2]]
+        timestamp = datetime.datetime.now().strftime("%m%d-%H%M")
+        write_results_tbf(events, corefs, afters_pred,parents,run_id="%s-%s" %(name.replace(" ","-"),timestamp))
+        precision,recall,f1 = precision_score(y_test,y_pred), recall_score(y_test,y_pred), f1_score(y_test,y_pred)
+        print("%s: %.4f %.4f %.4f" %(name,precision,recall,f1))
+
+        #score = clf.score(X_test, y_test)
+        #print("%s: %.4f" %(name,score))
         """
         clf.fit(X_train, y_train)
         score2 = clf.score(X_test, y_test)
@@ -226,7 +254,8 @@ if __name__ == "__main__":
 
     parser = OptionParser()
     parser.add_option('-m','--main',default=False,action="store_true",help='')
-
+    parser.add_option('-s','--statistics',default=False,action="store_true",help='')
+    parser.add_option('-d','--debug',default=False,action="store_true",help='')
     parser.add_option("-f", "--file", dest="filename",
                       help="write report to FILE", metavar="FILE")
     parser.add_option("-q", "--quiet",
@@ -237,5 +266,9 @@ if __name__ == "__main__":
     #import ipdb ; ipdb.set_trace()
     if options.main:
         main()
+    elif options.debug:
+        main(debug=True)
+    elif options.statistics:
+        stats()
     else:
         several_classifiers()
