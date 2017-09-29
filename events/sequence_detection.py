b418930b69f5d220405ca018c860d67f07d0dfe8
Çağıl Uluşahin <cagilulusahin@gmail.com>
2017-09-29 23:02:18 +0300

    stats v5

--- events/sequence_detection.py
+++ events/sequence_detection.py
@@ -204,15 +204,15 @@ names = ["Nearest Neighbors", "Linear SVM", "RBF SVM", "Gaussian Process",
          "Naive Bayes", "QDA"]
 classifiers = [
     KNeighborsClassifier(3),
-    #SVC(kernel="linear", C=0.025),
-    #SVC(gamma=2, C=1),
-    #GaussianProcessClassifier(1.0 * RBF(1.0), warm_start=True),
-    #DecisionTreeClassifier(max_depth=5),
-    #RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1),
-    #MLPClassifier(alpha=1),
-    #AdaBoostClassifier(),
+    SVC(kernel="linear", C=0.025),
+    SVC(gamma=2, C=1),
+    GaussianProcessClassifier(1.0 * RBF(1.0), warm_start=True),
+    DecisionTreeClassifier(max_depth=5),
+    RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1),
+    MLPClassifier(alpha=1),
+    AdaBoostClassifier(),
     GaussianNB(),
-    #QuadraticDiscriminantAnalysis()
+    QuadraticDiscriminantAnalysis()
 ]
 
 def get_stats(events, corefs, afters, parents,X_train,y_train,IDS):
