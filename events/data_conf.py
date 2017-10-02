f5bc1bf59ab529dff85c0868551a93ffda74c194
Çağıl Uluşahin <cagilulusahin@gmail.com>
2017-10-02 18:23:08 +0300

    when there is no ere folder

--- events/data_conf.py
+++ events/data_conf.py
@@ -1,14 +1,18 @@
 import os
 
 PROJECT_FOLDER=os.path.abspath(os.path.join(os.path.abspath(os.curdir), os.pardir))
-SOURCE_FOLDER = os.path.join(PROJECT_FOLDER,"data/LDC2017E02/data/2016/eval/eng/nw/source/")
-ERE_FOLDER = os.path.join(PROJECT_FOLDER,"data/LDC2017E02/data/2016/eval/eng/nw/ere/")
-ANN_FILELIST = os.listdir(ERE_FOLDER)
-SOURCE_FILELIST = os.listdir(SOURCE_FOLDER)
-ANN_FILENAME = lambda x:  os.path.join(ERE_FOLDER,ANN_FILELIST[x])
-SOURCE_FILENAME = lambda x:  os.path.join(SOURCE_FOLDER,SOURCE_FILELIST[x])
 DATAFILE = os.path.join(PROJECT_FOLDER,"data/datafile.txt")
 VOCABFILE = os.path.join(PROJECT_FOLDER,"data/vocab.txt")
+
+SOURCE_FOLDER = os.path.join(PROJECT_FOLDER,"data/LDC2017E02/data/2016/eval/eng/nw/source/")
+ERE_FOLDER = os.path.join(PROJECT_FOLDER,"data/LDC2017E02/data/2016/eval/eng/nw/ere/")
+try::
+    ANN_FILELIST = os.listdir(ERE_FOLDER)
+    SOURCE_FILELIST = os.listdir(SOURCE_FOLDER)
+    ANN_FILENAME = lambda x:  os.path.join(ERE_FOLDER,ANN_FILELIST[x])
+    SOURCE_FILENAME = lambda x:  os.path.join(SOURCE_FOLDER,SOURCE_FILELIST[x])
+except Exception as e:
+    print(e.args[0])
 
 event_type_index = {'Contact_Meet':0 ,
                     'Movement_Transport-Artifact':1 ,
